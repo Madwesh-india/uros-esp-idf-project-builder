a4c7e466fc6f0272e6ce3f062e2519c28ff9a88e
Madwesh-india <mawesh.1155@gmail.com>
2025-05-10 13:14:22 +0530

    simple imports

---
+++ configure.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import re
+
+TEMPLET_PATH = "./uRosTemplet"
+MICRO_ROS_REPO = "https://github.com/micro-ROS/micro_ros_espidf_component.git"
+CONFIG_FILE = "./uros_components_config.json"
+
